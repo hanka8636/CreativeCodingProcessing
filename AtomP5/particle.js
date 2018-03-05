af6199d376a5208890cbd3bc9fef73c35b233eca
hanka8636 <hanka8636@gmail.com>
2018-03-05 15:07:11 +0100

    fgrsdhyj

--- AtomP5/particle.js
+++ AtomP5/particle.js
@@ -1,27 +1,29 @@
-function Particle(x,y)
+class Particle
 {
-  this.pos = createVector(x, y);
-  this.vel = p5.Vector.random2D();
-   this.acc = createVector();
-   
-   this.update = function(){
-     this.pos.add(this.vel);
-     this.pos.add(this.acc);
+  constructor(x, y) {
+    this.pos = createVector(x, y);
+    this.vel = p5.Vector.random2D();
+    this.acc = createVector();
+  }
+
+  update(){
+    this.pos.add(this.vel);
+    this.pos.add(this.acc);
    }
    
-  this.show = function(){
-  stroke(255,25);
-  strokeWeight(1);
-  point(this.pos.x, this.pos.y);
+  show(){
+    stroke(255,25);
+    strokeWeight(1);
+    point(this.pos.x, this.pos.y);
   }
   
-  this.attracted = function(target){
+  attracted(target){
     // var dir = target - this.pos
-    var force = p5.Vector.sub(target, this.pos);
-    var d = force.mag();
+    const force = p5.Vector.sub(target, this.pos);
+    let d = force.mag();
     d = constrain(d, 1, 25)
-    var G = 50;
-    var magnitude = G/(d*d);
+    const G = 50;
+    const magnitude = G/(d*d);
     force.setMag(magnitude);
     this.acc.add(force);
   }
